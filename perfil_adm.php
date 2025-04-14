78543e7db66e3524435136c7e4238ed15059ddad
SarahVeiga <sarahveiga55@gmail.com>
2025-04-14 10:21:40 -0300

    locadora de veículos

--- perfil_adm.php
+++ perfil_adm.php
@@ -144,11 +144,30 @@
                                         <td>Uno</td>
                                         <td>ABC1D34</td>
                                         <td>
-                                            <span class="badge bg-success">Disponível ✅</span>
+                                            <span class="badge bg-success">Disponível</span>
                                         </td>
                                         <td>
                                             <div class="action-wrapper">
+                                                <form action="" class="btn-group-actions">
 
+                                                <!-- Botão Deletar (sempre disponível para 'Admin') -->
+
+                                                <button class="btn btn-danger btn-sm delete-btn"type="submit" name="deletar">
+                                                    Deletar
+                                                </button>
+
+                                                <!-- Botões condicionais -->
+                                                 <div class="rent-group">
+
+                                                <!-- Veículo alugado -->
+                                                 <button class="btn btn-warning btn-sm" type="submit" name="devolver">Devolver</button>
+
+                                                 <!-- Veiculo disponivel -->
+                                                <input type="number" name="dias" class="form-control" value="1" min="1" required>
+                                                <button class="btn btn-primary" name="alugar" type="submit">Alugar</button>
+
+                                                 </div>
+                                            </form>
                                         </div>
                                     </td>
                                 </tr>
